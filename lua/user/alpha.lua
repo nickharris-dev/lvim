72ede2f497b697222f71d6be7af6eeed19f13233
Nick Harris <nick@nickharris.dev>
2023-11-27 12:11:12 +0000

    Add Explorer to dashboard, seems to have gotten lost somewhere

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -1,12 +1,17 @@
 local kind = require("user.kind")
 
 table.insert(
-	lvim.builtin.alpha.dashboard.section.buttons.entries,
-	1,
-	{ "o", kind.icons.clock .. " Open last session", "<cmd>lua require('persistence').load()<cr>" }
+  lvim.builtin.alpha.dashboard.section.buttons.entries,
+  1,
+  { "o", kind.icons.clock .. " Open last session", "<cmd>lua require('persistence').load()<cr>" }
 )
 table.insert(
-	lvim.builtin.alpha.dashboard.section.buttons.entries,
-	2,
-	{ "g", kind.icons.git .. "  Git", "<cmd>lua require 'lvim.core.terminal'.lazygit_toggle()<cr>" }
+  lvim.builtin.alpha.dashboard.section.buttons.entries,
+  2,
+  { "e", kind.nvim_tree_icons.folder.empty_open .. "  Explorer", ":NvimTreeOpen<cr>" }
+)
+table.insert(
+  lvim.builtin.alpha.dashboard.section.buttons.entries,
+  3,
+  { "g", kind.icons.git .. "  Git", "<cmd>lua require 'lvim.core.terminal'.lazygit_toggle()<cr>" }
 )
